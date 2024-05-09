c973fc97b7e2a5172981091b019317128a7fecb1
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-05-09 17:07:21 -0500

    up

--- js/main.js
+++ js/main.js
@@ -11,22 +11,6 @@ const fbxUrl = 'models/saxenda.fbx';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log(buttonIntoVR)
-
-    setTimeout(() => {
-    
-        // Simula un clic en el botón
-        buttonIntoVR.click();
-    
-        // Agrega un evento 'click' al botón para detectar si se hace clic
-        buttonIntoVR.addEventListener('click', function(event) {
-            // Verifica si el evento se originó por una acción del usuario o no
-            if (event.isTrusted) {
-                console.log('El botón se ha clicado manualmente.');
-            } else {
-                console.log('El botón se ha clicado automáticamente.');
-            }
-        });
-    }, 1000); // Espera 1 segundo antes de simular el clic
 })
 
 // animation typedJS+
@@ -70,4 +54,14 @@ startingButton.addEventListener('click', () => {
     // mostramos la escena 
     document.querySelector('a-scene').style.display = 'block';
     document.getElementById('splash').style.display = 'none';
+    buttonIntoVR.click();
+
+    buttonIntoVR.addEventListener('click', function(event) {
+        // Verifica si el evento se originó por una acción del usuario o no
+        if (event.isTrusted) {
+            console.log('El botón se ha clicado manualmente.');
+        } else {
+            console.log('El botón se ha clicado automáticamente.');
+        }
+    });
 });
