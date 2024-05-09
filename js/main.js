ebc735bb0ced54200fb2508555122560fdc2f11b
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-05-09 12:18:17 -0500

    up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-const loader = document.getElementById('spinner');
-const textLoader = document.getElementById('textLoading');
+const spinner = document.getElementById('spinner');
+const textSpinner = document.getElementById('textLoading');
 const menu = document.getElementById('menu');
+
+const startingButton = document.getElementById('startingButton');
 
 const fbxUrl = 'models/saxenda.fbx';
 
 // inicializamos con un spinner
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        // escondemos el loading
-        loader.style.display = "none";
-        textLoader.style.display = "none";
+        spinner.classList.add('fadeOut');
+        textSpinner.classList.add('fadeOut');
 
-        // mostramos el menu
-        menu.style.display = "block"
-    }, 1000)
+        // Mostrar el menú con animación fadeIn después de que la animación de fadeOut haya terminado
+        setTimeout(() => {
+            // Esconder el loading
+            spinner.style.display = "none";
+            textSpinner.style.display = "none";
+
+            // Mostrar el menú con animación fadeIn
+            menu.style.display = "block"
+            menu.classList.add('fadeIn');
+        }, 500);
+    }, 800)
 })
+
+
+startingButton.addEventListener('click', () => {
+    // mostramos la escena 
+    document.querySelector('a-scene').style.display = 'block';
+    document.getElementById('splash').style.display = 'none';
+});
