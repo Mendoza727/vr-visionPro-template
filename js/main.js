0e75bcf8aeda164e08f09088174aaec991fcca16
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-05-09 16:59:03 -0500

    up

--- js/main.js
+++ js/main.js
@@ -13,8 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(buttonIntoVR)
 
     setTimeout(() => {
+        var buttonIntoVR = document.getElementById('myEnterVRButton');
+    
+        // Simula un clic en el botón
         buttonIntoVR.click();
-    }, 1)
+    
+        // Agrega un evento 'click' al botón para detectar si se hace clic
+        buttonIntoVR.addEventListener('click', function(event) {
+            // Verifica si el evento se originó por una acción del usuario o no
+            if (event.isTrusted) {
+                console.log('El botón se ha clicado manualmente.');
+            } else {
+                console.log('El botón se ha clicado automáticamente.');
+            }
+        });
+    }, 1000); // Espera 1 segundo antes de simular el clic
 })
 
 // animation typedJS+
