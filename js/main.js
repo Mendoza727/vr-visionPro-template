b6b43d2f29f6b072bdeaf25becf5db827427a38b
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-05-09 13:12:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,12 +7,10 @@ const startingButton = document.getElementById('startingButton');
 const fbxUrl = 'models/saxenda.fbx';
 
 // inicializamos con un spinner
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
+document.addEventListener('DOMContentLoaded', async() => {
+    setTimeout(async() => {
         spinner.classList.add('fadeOut');
         textSpinner.classList.add('fadeOut');
-
-        requestPermissions(); 
         // Mostrar el menú con animación fadeIn después de que la animación de fadeOut haya terminado
         setTimeout(() => {
             // Esconder el loading
@@ -33,30 +31,3 @@ startingButton.addEventListener('click', () => {
     document.getElementById('splash').style.display = 'none';
 });
 
-
-// permisos para immersive experience
-function requestPermissions() {
-    if ('xr' in navigator) {
-        navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
-            console.log('¿La sesión immersive-vr es compatible?', supported);
-            if (supported) {
-                // La sesión es compatible, puedes solicitar los permisos y continuar
-                console.log('Solicitar permisos...');
-            } else {
-                // La sesión no es compatible, manejar el caso en consecuencia
-                console.log('La sesión immersive-vr no es compatible con este dispositivo o navegador.');
-            }
-        }).catch((error) => {
-            // Manejar errores
-            console.error('Error al verificar la compatibilidad de la sesión immersive-vr:', error);
-        });
-    } else if (navigator.userAgent.includes('Chrome')) {
-        // Chrome
-        console.warn('La API de WebXR no está disponible en este navegador.');
-    } else if (navigator.userAgent.includes('Safari')) {
-        // Safari
-        console.warn('La API de WebXR no está disponible en este navegador.');
-    } else {
-        console.warn('La API de WebXR no está disponible en este navegador.');
-    }
-}
