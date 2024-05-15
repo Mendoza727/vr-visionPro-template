880806c618442bac573ba85fb3ade4e7f49cb8d4
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-05-15 10:56:18 -0500

    up

--- js/main.js
+++ js/main.js
@@ -10,48 +10,48 @@ const scene = document.getElementById("scene");
 const splash = document.getElementById("splash");
 
 // const gbl = 'models/jeringa-saxenda.glb';
-const loader = new THREE.GLTFLoader();
-document.addEventListener('DOMContentLoaded', () => {
-  loader.load(jeringa.getAttribute('src'), (gltf) => {
-    console.log(gltf.scene.children)
-  });
-})
+// const loader = new THREE.GLTFLoader();
+// document.addEventListener('DOMContentLoaded', () => {
+//   loader.load(jeringa.getAttribute('src'), (gltf) => {
+//     console.log(gltf.scene.children)
+//   });
+// })
 
 // animation typedJS+
-var typed2 = new Typed("#element", {
-    strings: [
-        "Cargando modelos...",
-        "Cargando texturas...",
-        "Preparando experiencia...",
-        "Cargando espacio...",
-        "iniciando...",
-    ],
-    typeSpeed: 30,
-    backSpeed: 0,
-    fadeOut: true,
-    backDelay: 3000,
-    loop: true,
-    showCursor: true,
-    onComplete: function (self) {
-        // Esta función se ejecuta cuando se completa la animación de Typed
-        self.stop();
+// var typed2 = new Typed("#element", {
+//     strings: [
+//         "Cargando modelos...",
+//         "Cargando texturas...",
+//         "Preparando experiencia...",
+//         "Cargando espacio...",
+//         "iniciando...",
+//     ],
+//     typeSpeed: 30,
+//     backSpeed: 0,
+//     fadeOut: true,
+//     backDelay: 3000,
+//     loop: true,
+//     showCursor: true,
+//     onComplete: function (self) {
+//         // Esta función se ejecuta cuando se completa la animación de Typed
+//         self.stop();
 
-        setTimeout(() => {
-            spinner.classList.add("fadeOut");
-            textSpinner.classList.add("fadeOut");
-            // Mostrar el menú con animación fadeIn después de que la animación de fadeOut haya terminado
-            setTimeout(() => {
-                // Esconder el loading
-                spinner.style.display = "none";
-                textSpinner.style.display = "none";
+//         setTimeout(() => {
+//             spinner.classList.add("fadeOut");
+//             textSpinner.classList.add("fadeOut");
+//             // Mostrar el menú con animación fadeIn después de que la animación de fadeOut haya terminado
+//             setTimeout(() => {
+//                 // Esconder el loading
+//                 spinner.style.display = "none";
+//                 textSpinner.style.display = "none";
 
-                // Mostrar el menú con animación fadeIn
-                menu.style.display = "block";
-                menu.classList.add("fadeIn");
-            }, 1);
-        }, 1);
-    },
-});
+//                 // Mostrar el menú con animación fadeIn
+//                 menu.style.display = "block";
+//                 menu.classList.add("fadeIn");
+//             }, 1);
+//         }, 1);
+//     },
+// });
 
 startingButton.addEventListener("click", () => {
     // mostramos la escena
